4c60da074a18434e0a567d86d704fc49f5bb96e1
kavas74 <92091249+kavas74@users.noreply.github.com>
2022-03-04 10:12:45 +0100

    Update 03.04(15 extra).py

--- 03.04(15 extra).py
+++ 03.04(15 extra).py
@@ -1,32 +1,13 @@
-lista = []
-paros = []
-paratlan = []
-i = 1
-while i < 6:
-    szam = int(input("Irj be egy számot: "))
-    if -99 <= szam <= 99:
-        print("Rendben!")
-        i += 1
-        lista.append(szam)
-        if (szam % 2 == 0):
-            paros.append(szam)
-        if (szam % 2 != 0):
-            paratlan.append(szam)
-    elif -99 > szam > 99:
-        print("Rossz")
-    else:
-        print("Ird be ujra a számot :")
+szo = input("Irj be valamit: ")
+
+x=set()
+
+vege = ''
+
+for i in szo:
+    if i not in x:
+        x.add(i)
+        vege=vege+str(szo.count(i))+i
 
 
-
-szamolas = len(paros)
-szamolas2 = len(paratlan)
-
-if szamolas == 0:
-    print("Nincs páros szám")
-
-if szamolas2 == 0:
-    print("Nincs páratlan szám")
-
-        
-print("{0} páros és {1} páratlan szám van".format(szamolas,szamolas2))
+print("A karakterekből ennyi darab van:" +vege)
